10d207f4158ae58c64b911ad2730821a988aaa74
risknu <145287544+risknu@users.noreply.github.com>
2024-04-29 15:19:40 +0200

    Added auto updating

--- ABOUT.md
+++ ABOUT.md
@@ -1,4 +1,4 @@
-`Kriet` is a server where you can build, fight, and do whatever you want, with complete freedom and more. (`1.20.1` - `forge`)
+`Kriet` is a server where you can build, fight, and do whatever you want, with complete freedom and more. (`1.20.1` - `forge`)(`kriet.duckdns.org:25605`)
 
 ## History
 The server was created on `4/28/2024` by two individuals, `gptdoktor` and `risknu`, with assistance from `тоффекс`, `Razenxc`, and others. The idea for the server originated from an incident involving administrator rights on the "Flopia" server, leading to the conception of an independent server where you can do anything you want!
--- cogs/server.py
+++ cogs/server.py
@@ -3,12 +3,33 @@ from __future__ import annotations
 from misc import server_cls, server_request
 from os import environ
 import disnake
-from disnake.ext import commands
+from disnake.ext import commands, tasks
 
 class server(commands.Cog):
     def __init__(self, bot: commands.Bot = None) -> None:
         self.bot: commands.Bot = bot
         self.__server_info: server_cls = server_request(environ.get("SERVER_IP"), environ.get("SERVER_PORT"))
+        self.update_status.start()
+        
+    @tasks.loop(seconds=30)
+    async def update_status(self):
+        try:
+            max: int | None = self.__server_info.max_players
+            now: int | None = self.__server_info.players_count
+            self.__server_info: server_cls = server_request(environ.get("SERVER_IP"), environ.get("SERVER_PORT"))
+            await self.bot.change_presence(status=disnake.Status.online, activity=disnake.Activity(
+                type=disnake.ActivityType.watching,
+                name=f'Working! {now if now is not None else "~"}/{max if max is not None else "~"}'))
+        except ConnectionRefusedError:
+            await self.bot.change_presence(status=disnake.Status.dnd, activity=disnake.Activity(
+                type=disnake.ActivityType.watching, name='Not Working!'))
+        except Exception as e:
+            await self.bot.change_presence(status=disnake.Status.idle, activity=disnake.Activity(
+                type=disnake.ActivityType.watching, name='Connection ERR!'))
+
+    @update_status.before_loop
+    async def before_update_status(self):
+        await self.bot.wait_until_ready()
         
     @commands.slash_command(
         description="With this command, you can check the server latency")
