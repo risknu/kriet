195f08562d5ed0a99784efa2a0d5e156a355ffe1
risknu <145287544+risknu@users.noreply.github.com>
2024-04-30 14:36:30 +0200

    Added requirements
    Added requirements to server also fixed bot.py script

--- bot.py
+++ bot.py
@@ -14,4 +14,5 @@ for ite, file in enumerate(listdir("./cogs/")):
     if not file.endswith('.py'): continue
     bot.load_extension(f"cogs.{file.split(".")[0]}")
 
-bot.run(environ.get("TOKEN")) 
+if __name__ == "__main__":
+    bot.run(environ.get("TOKEN")) 
