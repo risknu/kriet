9a4095ca3e7cbfa9cf38f4eab43ec91e647e05da
risknu <145287544+risknu@users.noreply.github.com>
2024-05-01 13:34:19 +0200

    Fixed texts and more
    Fxied texts impoverd show players and more

--- cogs/server.py
+++ cogs/server.py
@@ -9,6 +9,7 @@ from disnake.ext import commands, tasks
 class server(commands.Cog):
     def __init__(self, bot: commands.Bot = None) -> None:
         self.bot: commands.Bot = bot
+        self.__is_working_now: bool = False
         logging.getLogger('kriet').debug(f"Server send request to Java")
         self.__server_info: server_cls = server_request(environ.get("SERVER_IP"), environ.get("SERVER_PORT"))
         logging.getLogger('kriet').debug(f"Request is checked")
@@ -24,14 +25,17 @@ class server(commands.Cog):
             logging.getLogger('kriet').debug(f"Server send request to Java")
             self.__server_info: server_cls = server_request(environ.get("SERVER_IP"), environ.get("SERVER_PORT"))
             logging.getLogger('kriet').debug(f"Request is checked")
+            self.__is_working_now = True
             await self.bot.change_presence(status=disnake.Status.online, activity=disnake.Activity(
                 type=disnake.ActivityType.watching,
                 name=f'Working! {now if now is not None else "~"}/{max if max is not None else "~"}'))
         except ConnectionRefusedError:
             logging.getLogger('kriet').error(f"Not Working! Update; update_status => ConnectionRefusedError")
+            self.__is_working_now = False
             await self.bot.change_presence(status=disnake.Status.dnd, activity=disnake.Activity(
                 type=disnake.ActivityType.watching, name='Not Working!'))
         except Exception as e:
+            self.__is_working_now = False
             logging.getLogger('kriet').error(f"Not Working! Update; update_status => {e}")
             await self.bot.change_presence(status=disnake.Status.idle, activity=disnake.Activity(
                 type=disnake.ActivityType.watching, name='Connection ERR!'))
@@ -45,7 +49,7 @@ class server(commands.Cog):
     async def latency(self, inter: disnake.ApplicationCommandInteraction = None) -> None:
         latency: int | None = self.__server_info.latency
         logging.getLogger('kriet').debug(f"Account \"{inter.author.name}\" used command \"latency\".")
-        await inter.response.send_message(f"The server latency is approximately `{latency if latency is not None else "empty"}`")
+        await inter.response.send_message(f"The server latency is approximately `{round(float(latency)) if latency is not None else "~"}`")
         
     @commands.slash_command(
         description="With this command, you can view the list of players on the server")
@@ -54,14 +58,14 @@ class server(commands.Cog):
         players_names: int | None = self.__server_info.players_names
         max: int | None = self.__server_info.max_players
         now: int | None = self.__server_info.players_count
-        await inter.response.send_message(f"List of players on the server `{now if now is not None else "~"}`/`{max if max is not None else "~"}`\n{"\n".join(players_names) if players_names is not None else "empty"}")
+        await inter.response.send_message(f"List of players on the server `{now if now is not None else "~"}`/`{max if max is not None else "~"}`\n```diff\n{"\n".join([f'+ {player}' for player in players_names]) if players_names is not None else "- empty"}```")
         
     @commands.slash_command(
         description="With this command, you can check the game version")
     async def version(self, inter: disnake.ApplicationCommandInteraction = None) -> None:
         logging.getLogger('kriet').debug(f"Account \"{inter.author.name}\" used command \"version\".")
         version: str = self.__server_info.version
-        await inter.response.send_message(f"Server version is `{version if version is not None else "empty"}`")
+        await inter.response.send_message(f"Server version is `{version if version is not None else "x.y.z"}`")
         
     @commands.slash_command(
         description="With this command, you can view information about the game")
@@ -73,10 +77,11 @@ class server(commands.Cog):
         version: str = self.__server_info.version
         latency: int | None = self.__server_info.latency
         
-        version: str = f"server version is `{version if version is not None else "empty"}`;"
-        players: str = f"list of players on the server `{now if now is not None else "~"}`/`{max if max is not None else "~"}`\n{", ".join(players_names) if players_names is not None else "empty"};"        
-        latency: str = f"the server latency is approximately `{latency if latency is not None else "empty"}`;"
-        await inter.response.send_message(f"Server information; {version} {players} {latency}")
+        online_boolean: str = f"{"Working!" if self.__is_working_now else "Not Working!"}"
+        version: str = f"server version is `{version if version is not None else "x.y.z"}`;"
+        players: str = f"list of players on the server `{now if now is not None else "~"}`/`{max if max is not None else "~"}`\n{", ".join([f'`{player}`' for player in players_names]) if players_names is not None else "`empty`"};"        
+        latency: str = f"the server latency is approximately `{round(float(latency)) if latency is not None else "empty"}`;"
+        await inter.response.send_message(f"Status `{online_boolean}`; Server information; {version} {players} {latency}")
 
 def setup(bot: commands.Bot = None) -> None:
     bot.add_cog(server(bot))
